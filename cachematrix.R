e3393085a2012f66d821ca4d44f4efdef9931fdf
dguerrar <dguerrar@gmail.com>
2014-04-24 23:20:42 +0200

    versio 2

--- cachematrix.R
+++ cachematrix.R
@@ -9,15 +9,26 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
+		isNew<-FALSE
         set <- function(y) {
                 x <<- y
                 m <<- NULL
+				isNew<<-TRUE
+				message("setting new data to matrix")
         }
         get <- function() x
+
+		
         setinverse <- function(inverse) m <<- inverse
         getinverse <- function() m
+		
+		getisnew <- function() isNew
+		setisnew <- function(valor) isNew <<- valor
+
         list(set = set, get = get,
-             setinverse = setinverse,
+             setisnew = setisnew,
+			 getisnew = getisnew,
+			 setinverse = setinverse,
              getinverse = getinverse)
 }
 
@@ -31,12 +42,27 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 		m <- x$getinverse()
-        if(!is.null(m) ) {
+		isnew<-x$getisnew()
+        if(!is.null(m) && isnew==FALSE) {
                 message("getting cached inverse data")
                 return(m)
         }
         data <- x$get()
         m <- solve(data, ...)
         x$setinverse(m)
+		x$setisnew(FALSE)
         m
 }
+
+##in order to make som test
+##v<-matrix(nrow=2,ncol=2)
+##v[1,]<-c(1,2)
+##v[2,]<-c(2,2)
+##x<-makeCacheMatrix(v)
+##cacheSolve(x)
+
+
+##change data
+##v[2,]<-c(4,4)
+##x$set(v)
+##cacheSolve(x)
